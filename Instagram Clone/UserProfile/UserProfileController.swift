e8f2572bde2a464b6b83f6dc1d261c2153addae2
Mamun <abdullahamamun@Abdullahs-MacBook-Pro.local>
2018-01-04 19:10:46 -0500

    EP7

--- Instagram Clone/UserProfile/UserProfileController.swift
+++ Instagram Clone/UserProfile/UserProfileController.swift
@@ -11,6 +11,7 @@ import Firebase
 class UserProfileController: UICollectionViewController,UICollectionViewDelegateFlowLayout
 {
     var user :Users?
+    let cellId = "cellId"
     
     override func viewDidLoad()
     {
@@ -18,6 +19,7 @@ class UserProfileController: UICollectionViewController,UICollectionViewDelegate
         collectionView?.backgroundColor = .white
         collectionView?.register(UserProfileHeader.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: "headerId")
         fetchUser()
+        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
     }
     override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
         let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "headerId", for: indexPath) as! UserProfileHeader
@@ -27,6 +29,27 @@ class UserProfileController: UICollectionViewController,UICollectionViewDelegate
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
         return CGSize(width: view.frame.width, height: 200)
+    }
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
+    {
+        return 7
+    }
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
+    {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier:cellId, for: indexPath)
+        cell.backgroundColor = .purple
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize
+    {
+        let width = (view.frame.width-2)/3
+        return CGSize(width: width, height: width)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
     }
     fileprivate func fetchUser()  {
         guard let uid = Auth.auth().currentUser?.uid else{return}
--- Instagram Clone/UserProfile/UserProfileHeader.swift
+++ Instagram Clone/UserProfile/UserProfileHeader.swift
@@ -9,27 +9,105 @@
 import UIKit
 import Firebase
 class UserProfileHeader: UICollectionViewCell {
-    let profileImageView:UIImageView = {
-        let iv = UIImageView()
-        iv.backgroundColor = .red
-        
-        return iv
-    }()
+    
     var user:Users?
     {
         didSet
         {
-           setupProfileImage()
+            setupProfileImage()
+            usernameLabel.text = self.user?.userName
         }
     }
+    let profileImageView:UIImageView = {
+        let iv = UIImageView()
+       
+        
+        return iv
+    }()
+    let gridButton:UIButton =
+    {
+            let btn = UIButton(type: .system)
+            let img = UIImage(named: "grid")
+                btn.setImage(img, for: .normal)
+        //btn.tintColor = UIColor(white: 0, alpha: 0.1)
+        return btn
+    }()
+    let listButton:UIButton =
+    {
+        let btn = UIButton(type: .system)
+        let img = UIImage(named: "list")
+        btn.setImage(img, for: .normal)
+        btn.tintColor = UIColor(white: 0, alpha: 0.2)
+        return btn
+    }()
+    let bookmarkButton:UIButton =
+    {
+        let btn = UIButton(type: .system)
+        let img = UIImage(named: "ribbon")
+        btn.setImage(img, for: .normal)
+        btn.tintColor = UIColor(white: 0, alpha: 0.2)
+        return btn
+    }()
+    let usernameLabel:UILabel =
+    {
+        let label = UILabel()
+        //label.text = "Username"
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        return label
+    }()
+    let postsLabel:UILabel = {
+        let label = UILabel()
+        let attributedText = NSMutableAttributedString(string: "11\n", attributes: [NSAttributedStringKey.font:UIFont.boldSystemFont(ofSize: 14)])
+        attributedText.append(NSAttributedString(string: "posts", attributes: [NSAttributedStringKey.foregroundColor: UIColor.lightGray,NSAttributedStringKey.font:UIFont.boldSystemFont(ofSize: 14) ]))
+        label.attributedText = attributedText
+        label.textAlignment = .center
+        label.numberOfLines = 0 // display second line
+        return label
+    }()
+    let followersLabel:UILabel = {
+        let label = UILabel()
+        let attributedText = NSMutableAttributedString(string: "0\n", attributes: [NSAttributedStringKey.font:UIFont.boldSystemFont(ofSize: 14)])
+        attributedText.append(NSAttributedString(string: "followers", attributes: [NSAttributedStringKey.foregroundColor: UIColor.lightGray,NSAttributedStringKey.font:UIFont.boldSystemFont(ofSize: 14) ]))
+        label.attributedText = attributedText
+        label.textAlignment = .center
+         label.numberOfLines = 0 // display second line
+        return label
+    }()
+    let followingLabel:UILabel = {
+        let label = UILabel()
+        let attributedText = NSMutableAttributedString(string: "11\n", attributes: [NSAttributedStringKey.font:UIFont.boldSystemFont(ofSize: 14)])
+        attributedText.append(NSAttributedString(string: "following", attributes: [NSAttributedStringKey.foregroundColor: UIColor.lightGray,NSAttributedStringKey.font:UIFont.boldSystemFont(ofSize: 14) ]))
+        label.attributedText = attributedText
+        label.textAlignment = .center
+        label.numberOfLines = 0 // display second line
+        return label
+    }()
+    let editButtonProfile:UIButton = {
+         let btn = UIButton(type: .system)
+        btn.setTitle("Edit Profile", for: .normal)
+        btn.setTitleColor(.black, for: .normal)
+        btn.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        btn.layer.borderColor = UIColor.lightGray.cgColor // if we dont add this we wont be able to see border
+        btn.layer.borderWidth = 1 // specify the width of the border the defualt is 1
+        btn.layer.cornerRadius = 3
+        return btn
+        
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .blue
+        
         addSubview(profileImageView)
-        profileImageView.anchor(top: topAnchor, paddingTop: 12, left: leftAnchor, paddingLeft: 12, right: nil, padingRight: 0, bottom: nil, paddingBottom: 0, width: 80, height: 80)
+        profileImageView.anchor(top: self.topAnchor, paddingTop: 12, left: self.leftAnchor, paddingLeft: 12, right: nil, padingRight: 0, bottom: nil, paddingBottom: 0, width: 80, height: 80)
        
         profileImageView.layer.cornerRadius = 80/2
         profileImageView.layer.masksToBounds = true
+        setupBottomToolbar()
+        addSubview(usernameLabel)
+        usernameLabel.anchor(top: profileImageView.bottomAnchor, paddingTop: 4, left: self.leftAnchor, paddingLeft: 12, right: nil, padingRight: 0, bottom: gridButton.topAnchor, paddingBottom: 0, width: 0, height: 0)
+        setupUserStatsView()
+        addSubview(editButtonProfile)
+        editButtonProfile.anchor(top: postsLabel.bottomAnchor, paddingTop: 2, left: postsLabel.leftAnchor , paddingLeft: 0, right: followingLabel.rightAnchor, padingRight: 0, bottom: nil, paddingBottom: 0, width: 0, height: 34)
        
     }
     
@@ -57,4 +135,36 @@ class UserProfileHeader: UICollectionViewCell {
             }.resume()
        
     }
+    fileprivate func setupBottomToolbar()
+    {
+        let topDividerView = UIView()
+        topDividerView.backgroundColor = UIColor.lightGray
+        let bottomSeparatorView = UIView()
+        bottomSeparatorView.backgroundColor = UIColor.lightGray
+        
+        let stackView = UIStackView(arrangedSubviews: [gridButton,listButton,bookmarkButton])
+        addSubview(stackView)
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.anchor(top: nil, paddingTop: 0, left: leftAnchor, paddingLeft: 0, right: rightAnchor, padingRight: 0, bottom: self.bottomAnchor, paddingBottom: 0, width: 0, height: 50)
+        addSubview(topDividerView)
+        addSubview(bottomSeparatorView)
+        topDividerView.anchor(top: stackView.topAnchor, paddingTop: 0, left: leftAnchor, paddingLeft: 0, right: rightAnchor, padingRight: 0, bottom: nil, paddingBottom: 0, width: 0, height: 0.5)
+        bottomSeparatorView.anchor(top: nil, paddingTop: 0, left: leftAnchor, paddingLeft: 0, right: rightAnchor, padingRight: 0, bottom: stackView.bottomAnchor, paddingBottom: 0, width: 0, height: 0.5)
+    }
+    fileprivate func setupUserStatsView()
+    {
+        let stackView = UIStackView(arrangedSubviews: [postsLabel,followersLabel,followingLabel])
+        addSubview(stackView)
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.anchor(top: topAnchor, paddingTop: 12, left: profileImageView.rightAnchor, paddingLeft: 12, right: rightAnchor, padingRight: 12, bottom: nil, paddingBottom: 0, width: 0, height: 50)
+        
+    }
+    
+    
+    
+    
+    
+    
 }
